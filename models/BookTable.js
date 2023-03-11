8517221c8f7272a712532773e95ed964e64b1348
debarkamondal <debarkamondal@gmail>
2023-03-12 01:34:23 +0530

    Added schema for booking table

---
+++ models/BookTable.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const bookTableSchema = new Schema({
+  table: {
+    type: Number,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    max: [9999999999, "The entered number is invalid"],
+    min: [6000000000, "The entered number is invalid"],
+    unique: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("bookTable", bookTableSchema);
