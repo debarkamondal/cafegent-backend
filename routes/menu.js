a32fbba7268f1c96a5ad419bc2f1a6ce762ace36
debarkamondal <debarkamondal@gmail>
2023-03-14 10:26:39 +0530

    adding on update menu api

--- routes/menu.js
+++ routes/menu.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const menuItems = require("../models/menuItems");
-const router = express.Router();
-
 /**
  * ! This API endpoint is only to be used by admin.
  * TODO: add JWT validation so that only admin can use the endpoint
  */
+
+const express = require("express");
+const menuItems = require("../models/menuItems");
+const router = express.Router();
 
 //---------------------------------------------------Creating a new menu item-----------------------------------------------------//
 router.post("/addItem", async (req, res) => {
@@ -23,10 +23,28 @@ router.post("/addItem", async (req, res) => {
   }
 });
 
+//-----------------------------------------------------Editing a menu item-------------------------------------------------------//
+
+router.put("/edititem", async (req, res) => {
+  try {
+    const newMenuItem = await menuItems.findById(req.body._id);
+    for (key in req.body) {
+      if (key === "_id") continue;
+      console.log(newMenuItem.key);
+      req.body.key ? (newMenuItem.key = req.body.key) : null;
+    }
+    newMenuItem.save();
+    res.status(200).json(newMenuItem);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//------------------------------------------------------Deleting menu item--------------------------------------------------------//
 router.delete("/deleteitem/:id", async (req, res) => {
   try {
-    const menuItem = await menuItems.findByIdAndDelete(req.params.id);
-    res.status(200).json(menuItem);
+    const newMenuItem = await menuItems.findByIdAndDelete(req.params.id);
+    res.status(200).json(newMenuItem);
   } catch {
     console.log("Item not found");
   }
