583f2546f0325102891292820a1904e551cade05
debarkamondal <debarkamondal@gmail.com>
2023-04-05 02:09:04 +0530

    menuItem schema modified

--- models/menuItems.js
+++ models/menuItems.js
@@ -2,25 +2,33 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const menuItemsSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  tag: {
-    type: String,
-    Enum: ["Veg", "Non-Veg"],
-    required: true,
-  },
-  genre: {
-    type: String,
-    Enum: ["Starters", "Chinese", "Indian", "Beverages", "Breads", "Snacks"],
-    required: true,
-  },
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	tag: {
+		type: String,
+		Enum: ["Veg", "Non-Veg"],
+		required: true,
+	},
+	genre: {
+		type: String,
+		Enum: ["Starters", "Chinese", "Indian", "Beverages", "Breads", "Snacks"],
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		required: true,
+	},
 });
 
 module.exports = mongoose.model("menuItems", menuItemsSchema);
